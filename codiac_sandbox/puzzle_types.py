f82116112cec2d779d443caebfa129206be3886e
Liam Whitenack <liamwhitenack@outlook.com>
2025-07-08 16:38:51 -0400

    made the puzzle types have the "used" attribute

--- codiac_sandbox/puzzle_types.py
+++ codiac_sandbox/puzzle_types.py
@@ -12,6 +12,7 @@ class CryptographBase(ABC):
         string_to_encrypt: str,
         puzzle_type: str = "Undefined",
         hints: list[HintBase] | None = None,
+        used: bool = False,
     ) -> None:
         if hints is None:
             hints = []
@@ -23,6 +24,7 @@ class CryptographBase(ABC):
         )
         random.shuffle(letters)
         self.hints = hints + [GiveALetterHint(letter) for letter in letters]
+        self.used = used
 
     def to_json(self, all_info: bool = False) -> dict[str, list | str | dict[str, str]]:
         res = self.__dict__.copy()
@@ -41,7 +43,8 @@ class CryptographBase(ABC):
                 encryption_map=None,
             )
         else:
-            res["hints"] = [hint.to_json() for hint in res["hints"]]
+            hints = [hint.to_json() for hint in self.hints]
+            res |= dict(hints=hints, used=None)  # type: ignore[attr-defined]
 
         return {k: str(v) for k, v in res.items() if v is not None}
 
@@ -53,13 +56,17 @@ class CryptographBase(ABC):
 
 
 class ListPuzzle(CryptographBase):
-    def __init__(self, setup: str, elements: list[str]) -> None:
-        super().__init__(" ".join(elements), "list")
+    def __init__(self, setup: str, elements: list[str], used: bool = False) -> None:
+        super().__init__(" ".join(elements), "list", used=used)
         self.setup = setup
 
     @classmethod
     def from_json(cls, data: dict[str, Any]) -> Self:
-        return cls(setup=data["setup"], elements=data["string_to_encrypt"].split())
+        return cls(
+            setup=data["setup"],
+            elements=data["string_to_encrypt"].split(),
+            used=data["used"],
+        )
 
 
 class CharacterQuote(CryptographBase):
@@ -70,8 +77,9 @@ class CharacterQuote(CryptographBase):
         character_name: str,
         source: str,
         release_date: str,
+        used: bool = False,
     ):
-        super().__init__(quote, f"{source_type} Quote")
+        super().__init__(quote, f"{source_type} Quote", used=used)
         self.character_name = character_name
         self.source = source
         self.release_date = release_date
@@ -84,31 +92,48 @@ class CharacterQuote(CryptographBase):
             character_name=data["character_name"],
             source=data["source"],
             release_date=data["release_date"],
+            used=data["used"],
         )
 
 
 class FamousDocumentQuote(CryptographBase):
-    def __init__(self, quote: str, source: str, author: str, release_date: str):
-        super().__init__(quote, "Famous Document")
+    def __init__(
+        self,
+        quote: str,
+        source: str,
+        author: str,
+        release_date: str,
+        used: bool = False,
+    ):
+        super().__init__(quote, "Famous Document", used=used)
         self.source = source
         self.author = author
         self.release_date = release_date
 
     @classmethod
-    def from_json(cls, data: dict[str, Any]) -> Self:
+    def from_json(
+        cls,
+        data: dict[str, Any],
+        used: bool = False,
+    ) -> Self:
         return cls(
             quote=data["string_to_encrypt"],
             source=data["source"],
             author=data["author"],
             release_date=data["release_date"],
+            used=data["used"],
         )
 
 
 class DirectQuote(CryptographBase):
     def __init__(
-        self, quote: str, author: str, release_date: str | None = None
+        self,
+        quote: str,
+        author: str,
+        release_date: str | None = None,
+        used: bool = False,
     ) -> None:
-        super().__init__(quote, "Direct Quote")
+        super().__init__(quote, "Direct Quote", used=used)
         self.author = author
         self.release_date = release_date
 
@@ -118,21 +143,33 @@ class DirectQuote(CryptographBase):
             quote=data["string_to_encrypt"],
             author=data["author"],
             release_date=data.get("release_date"),
+            used=data["used"],
         )
 
 
 class GeneralPhrase(CryptographBase):
-    def __init__(self, phrase: str):
-        super().__init__(phrase, "General Quote")
+    def __init__(
+        self,
+        phrase: str,
+        used: bool = False,
+    ):
+        super().__init__(phrase, "General Quote", used=used)
 
     @classmethod
     def from_json(cls, data: dict[str, Any]) -> Self:
-        return cls(phrase=data["string_to_encrypt"])
+        return cls(phrase=data["string_to_encrypt"], used=data["used"])
 
 
 class SongLyrics(CryptographBase):
-    def __init__(self, lyrics: str, artist: str, title: str, release_date: str) -> None:
-        super().__init__(lyrics, "Song lyrics")
+    def __init__(
+        self,
+        lyrics: str,
+        artist: str,
+        title: str,
+        release_date: str,
+        used: bool = False,
+    ) -> None:
+        super().__init__(lyrics, "Song lyrics", used=used)
         self.artist = artist
         self.title = title
         self.release_date = release_date
@@ -144,12 +181,18 @@ class SongLyrics(CryptographBase):
             artist=data["artist"],
             title=data["title"],
             release_date=data["release_date"],
+            used=data["used"],
         )
 
 
 class Riddle(CryptographBase):
-    def __init__(self, question: str, answer: str) -> None:
-        super().__init__(answer, "Riddle")
+    def __init__(
+        self,
+        question: str,
+        answer: str,
+        used: bool = False,
+    ) -> None:
+        super().__init__(answer, "Riddle", used=used)
         self.question = question
 
     @classmethod
@@ -157,19 +200,24 @@ class Riddle(CryptographBase):
         return cls(
             question=data["question"],
             answer=data["string_to_encrypt"],
+            used=data["used"],
         )
 
 
 class RiddleSolvedInReverse(CryptographBase):
-    def __init__(self, question: str, answer: str) -> None:
-        super().__init__(question, "Reverse Riddle")
+    def __init__(
+        self,
+        question: str,
+        answer: str,
+        used: bool = False,
+    ) -> None:
+        super().__init__(question, "Reverse Riddle", used=used)
         self.answer = answer
 
     @classmethod
     def from_json(cls, data: dict[str, Any]) -> Self:
         return cls(
-            question=data["string_to_encrypt"],
-            answer=data["answer"],
+            question=data["string_to_encrypt"], answer=data["answer"], used=data["used"]
         )
 
 
